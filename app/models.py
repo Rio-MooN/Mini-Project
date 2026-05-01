97ee6216df613fa8069b4a9114b8b193d877ff22
Rio-MooN <imoimo0093@gmail.com>
2026-05-01 02:37:28 +0000

    project initial setting
    이슈 탬플릿 간략히 추가
    
    add issue templates
    git push origin feature/issue-template
    )
    
    Update issue templates
    
    chore: add .env.example and auto-copy in postCreateCommand
    )
    
    Update database.py
    
    Update database.py
    
    Rename requirement.txt to requirements.txt
    
    Create models.py
    
    Update docker-compose.yml
    
    Rename DockerFile to Dockerfile
    
    nano
    
    nano-re

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from app.db.database import Base
+from sqlalchemy import Column, Integer, String
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
